ae8bc8465b09ddc3317a127c9a0be670e905bf4b
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-07-29 13:35:56 +0200

    Modifies Dockerfile
    Co-authored-by: r0binsahin <robin.sahin88@gmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>

--- build/app.js
+++ build/app.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get("/", (req, res) => {
-    res.send("Hello From Express and Typescirpt");
+const port = 8080;
+app.get("/status", (req, res) => {
+    res.send("Hello from Rookies").status(200);
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
