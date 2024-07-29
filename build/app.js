24ea19c0b433e0b4de0916b4961fba3eb1c7d30f
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-07-29 15:05:47 +0200

    Creates new endpoints
    Co-authored-by: r0binsahin <robin.sahin88@gmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>

--- build/app.js
+++ build/app.js
@@ -4,10 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const zod_1 = require("zod");
+const PayloadSchema = zod_1.z.object({
+    carId: zod_1.z.string().uuid(),
+    amount: zod_1.z.number().int(),
+});
 const app = (0, express_1.default)();
 const port = 8080;
+app.get("/", (req, res) => {
+    res.send("Hello from Index").status(200);
+});
 app.get("/status", (req, res) => {
     res.send("Hello from Rookies").status(200);
+});
+app.post("/payments", (req, res) => {
+    const result = PayloadSchema.safeParse(req.body);
+    const { carId, amount } = result.data;
+    if (!carId || !amount) {
+        res.status(400).send("Invalid request");
+    }
+    res.status(200).send("Payment success");
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,37 @@
-import Express from "express";
+import Express, { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+
+const PayloadSchema = z.object({
+  carId: z.string().uuid(),
+  amount: z.number().int(),
+});
+
+type PaymentPayload = {
+  carId: string;
+  amount: number;
+};
 
 const app = Express();
 const port = 8080;
 
-app.get("/status", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Index").status(200);
+});
+
+app.get("/status", (req: Request, res: Response) => {
   res.send("Hello from Rookies").status(200);
+});
+
+app.post("/payments", (req: Request, res: Response) => {
+  const result = PayloadSchema.safeParse(req.body);
+
+  const { carId, amount } = result.data as PaymentPayload;
+
+  if (!carId || !amount) {
+    res.status(400).send("Invalid request");
+  }
+  res.status(200).send("Payment success");
 });
 
 app.listen(port, () => {
