e89ce5aa8c61208589721a7b9e08e396cbe25228
r0binsahin <robin.sahin88@gmail.com>
2024-07-30 13:40:38 +0200

    winston logger added
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- build/app.js
+++ build/app.js
@@ -4,20 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const logger_1 = require("./logger");
 const app = (0, express_1.default)();
 const port = 8000;
 app.use(express_1.default.json());
-app.get("/", (req, res) => {
-    res.status(200).send("Hello from Index1");
+app.get('/', (_req, res) => {
+    res.status(200).send('Hello from Index1');
+    logger_1.logger.info('Index page!');
 });
-app.get("/status", (req, res) => {
-    res.status(200).send("Hello from Rookies");
+app.get('/status', (_req, res) => {
+    res.status(200).send('Hello from Rookies');
+    logger_1.logger.info('Status page!');
 });
-app.post("/payments", (req, res) => {
+app.post('/payments', (req, res) => {
     const { carId, amount } = req.body;
     console.log(carId);
     console.log(amount);
-    res.status(200).json("Payment success");
+    logger_1.logger.log({
+        message: 'Invalid amount. It must be an integer.',
+        level: 'error',
+    });
+    res.status(200).json('Payment success');
+    logger_1.logger.info({
+        level: 'info',
+        message: 'Payment processed successfully',
+        carId,
+        amount,
+    });
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,39 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import { logger } from './logger';
 
 const app = express();
 const port = 8000;
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello from Index1");
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).send('Hello from Index1');
+
+  logger.info('Index page!');
 });
 
-app.get("/status", (req: Request, res: Response) => {
-  res.status(200).send("Hello from Rookies");
+app.get('/status', (_req: Request, res: Response) => {
+  res.status(200).send('Hello from Rookies');
+
+  logger.info('Status page!');
 });
 
-app.post("/payments", (req: Request, res: Response) => {
+app.post('/payments', (req: Request, res: Response) => {
   const { carId, amount } = req.body;
   console.log(carId);
   console.log(amount);
 
-  res.status(200).json("Payment success");
+  logger.log({
+    message: 'Invalid amount. It must be an integer.',
+    level: 'error',
+  });
+
+  res.status(200).json('Payment success');
+  logger.info({
+    level: 'info',
+    message: 'Payment processed successfully',
+    carId,
+    amount,
+  });
 });
 
 app.listen(port, () => {
