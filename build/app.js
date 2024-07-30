f7eb25626e87aaabf44a8dd31bd0e280f576f623
r0binsahin <robin.sahin88@gmail.com>
2024-07-30 14:47:00 +0200

    builds the app
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- build/app.js
+++ build/app.js
@@ -20,6 +20,15 @@ app.post('/payments', (req, res) => {
     const { carId, amount } = req.body;
     console.log(carId);
     console.log(amount);
+    if (typeof amount !== 'number' || !Number.isInteger(amount)) {
+        logger_1.logger.log({
+            message: 'Invalid amount. It must be an integer.',
+            level: 'error',
+        });
+        return res
+            .status(400)
+            .json({ error: 'Invalid amount. It must be an integer.' });
+    }
     const random = Math.random();
     if (random > 0.5) {
         logger_1.logger.log({
