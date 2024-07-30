d8dbf8643f6f6fffb2dc4f9b6d9ba4ac01335342
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-07-30 11:43:47 +0200

    removes bunyan
    Co-authored-by: r0binsahin <robin.sahin88@gmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,25 @@
 import express, { Request, Response } from "express";
-import lb from "@google-cloud/logging-bunyan";
 
-async function startServer() {
-  try {
-    const { logger, mw } = await lb.express.middleware({
-      logName: "samples_express",
-    });
+const app = express();
+const port = 8000;
+app.use(express.json());
 
-    const app = express();
-    const port = 8080;
-    app.use(express.json());
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello from Index1");
+});
 
-    app.use(mw);
+app.get("/status", (req: Request, res: Response) => {
+  res.status(200).send("Hello from Rookies");
+});
 
-    app.get("/", (req: Request, res: Response) => {
-      res.status(200).send("Hello from Index1");
-      logger.info("Visited index page");
-    });
+app.post("/payments", (req: Request, res: Response) => {
+  const { carId, amount } = req.body;
+  console.log(carId);
+  console.log(amount);
 
-    app.get("/status", (req: Request, res: Response) => {
-      res.status(200).send("Hello from Rookies");
-    });
+  res.status(200).json("Payment success");
+});
 
-    app.post("/payments", (req: Request, res: Response) => {
-      const { carId, amount } = req.body;
-      console.log(carId);
-      console.log(amount);
-
-      logger.info("Payment received:");
-      res.status(200).json("Payment success");
-    });
-
-    app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`);
-      logger.info(`Server started at http://localhost:${port}`);
-    });
-  } catch (error) {
-    console.error("Error initializing server:", error);
-  }
-}
-
-startServer();
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
